c2895916b9c8e0192a3fb6bfd7b8f109408a495f
zeckem19 <zeckem19@gmail.com>
2020-11-02 23:54:25 +0800

    finished coin change prioblem

--- PythonAlgoCode/DynamicProgramming/coinchange_zm.py
+++ PythonAlgoCode/DynamicProgramming/coinchange_zm.py
@@ -15,15 +15,16 @@ class CoinChange:
 	def dynamicProgrammingApproach(self, coins, M):
 	
 		dpTable = [[0 for i in range(M+1)] for coin in range(len(coins)+1) ]
-        
-        for i in range(len(coins)+1):
-            dpTable[i][0] = 1
-        for coinIdx in range(1, len(coins)+1):
-            for val in range(1, M+1):
-                if coinIdx > M:
-                    dpTable[coinIdx][val] = dpTable[coinIdx-1][val] 
-                else:
-                    id 
+		
+		for i in range(len(coins)+1):
+			dpTable[i][0] = 1
+		for coinIdx in range(1, len(coins)+1):
+			for val in range(1, M+1):
+				if coins[coinIdx-1] > M:
+					dpTable[coinIdx][val] = dpTable[coinIdx-1][val] 
+				else:
+					dpTable[coinIdx][val] = dpTable[coinIdx-1][val] + dpTable[coinIdx][val-coins[coinIdx-1]]
+		print(dpTable[-1][-1])
 
 
 if __name__ == "__main__":
